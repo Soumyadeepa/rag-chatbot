a2a0f24bae1d5e898c68fff4a6ffd822ff3657d0
Soumyadeepa Dutta <soumyadeepa2003@gmail.com>
2026-06-01 02:00:24 +0530

    Frontend backend integration test

--- main.py
+++ main.py
@@ -1,71 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from dotenv import load_dotenv
-
-from langchain_groq import ChatGroq
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_chroma import Chroma
-
-load_dotenv()
 
 app = FastAPI()
 
-# Request model
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class ChatRequest(BaseModel):
     question: str
 
-# Embedding model
-embedding = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-
-# Load ChromaDB
-db = Chroma(
-    persist_directory="chroma_db",
-    embedding_function=embedding
-)
-
-# Load Groq
-llm = ChatGroq(
-    model="llama-3.3-70b-versatile"
-)
 
 @app.get("/")
 def home():
-    return {"message": "RAG Chatbot Running"}
+    return {
+        "message": "Backend Running Successfully"
+    }
+
 
 @app.post("/chat")
 def chat(request: ChatRequest):
 
-    question = request.question
-
-    # Retrieve top 3 relevant chunks
-    docs = db.similarity_search(
-        question,
-        k=3
-    )
-
-    # Create context
-    context = "\n\n".join(
-        [doc.page_content for doc in docs]
-    )
-
-    prompt = f"""
-Answer ONLY from the provided context.
-
-If the answer is not present in the context,
-say:
-"I could not find that information in the document."
-
-Context:
-{context}
-
-Question:
-{question}
-"""
-
-    response = llm.invoke(prompt)
-
     return {
-        "answer": response.content
+        "answer": f"You asked: {request.question}"
     }
