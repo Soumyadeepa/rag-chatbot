338ffa9918c66c32eb171089ee41ce3dea1f0ea0
Soumyadeepa Dutta <soumyadeepa2003@gmail.com>
2026-06-02 13:03:37 +0530

    Completed RAG chatbot

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from dotenv import load_dotenv
+
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -16,16 +23,60 @@ class ChatRequest(BaseModel):
     question: str
 
 
+# Embedding Model
+embedding = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# Load Existing Chroma Database
+db = Chroma(
+    persist_directory="chroma_db",
+    embedding_function=embedding
+)
+
+# Groq LLM
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile"
+)
+
+
 @app.get("/")
 def home():
     return {
-        "message": "Backend Running Successfully"
+        "message": "RAG Chatbot Running"
     }
 
 
 @app.post("/chat")
 def chat(request: ChatRequest):
 
+    question = request.question
+
+    docs = db.similarity_search(
+        question,
+        k=3
+    )
+
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
+
+    prompt = f"""
+Answer ONLY from the provided context.
+
+If the answer is not present in the context,
+say:
+"I could not find that information in the document."
+
+Context:
+{context}
+
+Question:
+{question}
+"""
+
+    response = llm.invoke(prompt)
+
     return {
-        "answer": f"You asked: {request.question}"
+        "answer": response.content
     }
